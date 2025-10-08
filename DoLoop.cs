e3e347079fc13368558f2d867613522effba6642
pubgrub2 <admin@pubgrub2.de>
2025-10-08 11:52:56 +0200

    ideas

---
+++ DoLoop.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+public class DoLoop
+{
+	public Initialize(string loopContent)
+}
---
+++ Roboterwelt_VLG/InstructionSet.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roboterwelt_VLG
+{
+    class InstructionSet
+    {
+        string instructions;
+        public InstructionSet(string s)
+        {
+            instructions = s;
+        }
+        public void Execute()
+        {
+
+        }
+        public void MakeExecutionOrder()
+        {
+
+        }
+    }
+}
